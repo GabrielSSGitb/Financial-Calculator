f51408b77717bca763e12ffb0ef3c3341e9970d0
Gabriel SS <gabrielsantosgithubac@gmail.com>
2025-04-18 20:21:04 -0300

    Adding Files

---
+++ src/financialcalculator/FXMLDocumentController.java
@@ -0,0 +1,26 @@
+package financialcalculator;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class FXMLDocumentController implements Initializable {
+    public int t = 0;
+    @FXML
+    private TextField txtScreen;
+    @FXML
+    private void Calculate(ActionEvent event) {
+       
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
