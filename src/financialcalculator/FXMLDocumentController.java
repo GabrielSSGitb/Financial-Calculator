d3901720faf8a977e374acc2bba601c92adbb375
Gabriel SS <gabrielsantosgithubac@gmail.com>
2025-04-24 21:57:43 -0300

    Addup Function

--- src/financialcalculator/FXMLDocumentController.java
+++ src/financialcalculator/FXMLDocumentController.java
@@ -1,30 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package financialcalculator;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author gabri
  */
 public class FXMLDocumentController implements Initializable {
+    ArrayList<Integer> array = new ArrayList<>(); //Creating an arrayList
+    //Apply JSON here!!
+    
     
     @FXML
     private Label label;
-    
+    @FXML
+    private TextField txtScreen;
     @FXML
     private void handleButtonAction(ActionEvent event) {
-       label.setText("Worked!");
+        //Enter btn
+        String addValue = txtScreen.getText();
+        array.add(Integer.parseInt(addValue));
     }
+    @FXML
+    private void addUp(ActionEvent event) {
+        //Addup calculating:
+        int result = 0;
+        for(int i = 0; i < array.size(); i++) {
+            result += array.get(i);
+        };
+        
+        txtScreen.setText(Integer.toString(result));
+    };
+    @FXML
+    private void btn(ActionEvent event) {
+        //Start adding buttons
+    };
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
